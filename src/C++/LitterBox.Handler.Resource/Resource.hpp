5c44af44962dc6ea728f3ac5848cd23e039aafeb
Jordan (Froz) <byfroz@outlook.com>
2019-05-16 18:51:17 -1000

    Create Resource.hpp

---
+++ src/C++/LitterBox.Handler.Resource/Resource.hpp
@@ -0,0 +1,63 @@
+/*
+ * Copyright (c) 2019 M.I.T. License
+ *
+ * This is LitterBox's system resources
+ * header file -- containing access to 
+ * things such as system time, memory
+ * allocation, and much more.
+ *
+ * Author(s):
+ * + Froz
+*/
+
+#ifndef _RESOURCES_HPP_
+#define _RESOURCES_HPP_
+
+#include <cstdint>
+#include <cstdib>
+#include <cstdlib>
+
+namespace LitterBox
+{
+    struct Heap
+    {
+    public:
+        static void* Allocate(unsigend bytes) noexcept; // This function can throw many errors, but I told C++ to snooze them.
+        static uintptr_t Deallocate(void &pointer) noexcept; // Likewise, here.
+    };
+    
+    void* Heap::Allocate(unsigned bytes) noexcept
+    {
+        int *ptr = nullptr;
+        ptr = static_cast<int*>(malloc(bytes));
+        if (ptr != NULL)
+            return ptr;
+        else
+            return nullptr;
+    }
+    
+    uintptr_t Heap::Deallocate(void &pointer) noexcept
+    {
+        if (pointer == NULL)
+            return pointer;
+        else
+        {
+            free(pointer);
+            return 0;
+        }
+    }
+    
+    struct Time
+    {
+    public:
+        static auto GetCurrentTime() decltype(std::chrono::time_point<std::chrono::system_clock>);
+    };
+    
+    auto Time::GetCurrentTime() decltype(std::chrono::time_point<std::chrono::system_clock>) ->
+    {
+        auto time = std::chrono::system_clock::now();
+        return time;
+    }
+} // namespace LitterBox
+
+#endif // _RESOURCES_HPP_
